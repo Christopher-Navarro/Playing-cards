f0baee760344fe11083a2585e8fc54eefef10f12
Christopher Navarro <navarro.christopher16@yahoo.com>
2023-09-02 23:30:41 -0700

    upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Deck from "./Deck";
+
+function App() {
+  return (
+    <div className="App">
+      <Deck />
+    </div>
+  );
+}
+
+export default App;
